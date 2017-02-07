179135b9ad666efde72f5944e34ebc31e7d6a281
Eric Kudler <joeento@gmail.com>
2017-02-07 11:34:04 -0500

    Added an extra function to abstract any actions taken by the message without effecting what it returns

--- message.js
+++ message.js
@@ -1,17 +1,21 @@
 'use strict';
 
-var Message = function() {
+function Message() {
 	this.question = '';
 	this.reply = '';
-};
+}
 Message.prototype.process = function(answer_string) {
 	return answer_string;
 };
-Message.prototype.response = function(answer_string) {
+Message.prototype.react = function(data) {
+	return false;
+};
+Message.prototype.answer = function(answer_string) {
 	var data = this.process(answer_string);
 	if (!data) {
 		this.reply = 'I\'m sorry, I\'ve gotten confused.  Let\'s start over';
 		return 0;
 	}
+	return this.react(data);
 };
 module.exports = Message;
