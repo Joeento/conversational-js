c2e0cd35920f9abbf15a2525bed12ceaa8e6fcbe
Eric Kudler <joeento@gmail.com>
2017-02-08 14:16:27 -0500

    Added function for importing messages array into conversation

--- conversation.js
+++ conversation.js
@@ -1,11 +1,23 @@
 'use strict';
+var Message = require('./message');
 
 function Conversation() {
 	this.state = 1;
 	this.Conversations = [];
 }
+
 Conversation.prototype.getQuestion = function() {
 	return this.messages[this.state].question;
+};
+
+Conversation.prototype.loadMessages = function(message_collection) {
+	for (var i = 0; i < message_collection.length; i++) {
+		var message = new Message();
+		for(var prop in message_collection[i]) {
+			message[prop] = message_collection[i][prop];
+		}
+		this.messages[message_collection[i].id] = message;
+	}
 };
 
 module.exports = Conversation;
